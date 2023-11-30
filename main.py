a6cc18a42b8258941a0fc77633c2eb008d2bbae1
bhattshubham1401 <shubhambhatt687@gmail.com>
2023-11-30 23:39:52 +0530

    main

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 app.register_blueprint(router)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8081)
 
