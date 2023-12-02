0e8ebd11dc98240e78a653a66bd0b98bf501f7af
bhattshubham1401 <shubhambhatt687@gmail.com>
2023-12-02 15:39:31 +0530

    actual data

--- config/db.py
+++ config/db.py
@@ -1,25 +1,16 @@
 from pymongo.mongo_client import MongoClient
-from urllib.parse import quote_plus
 
-# Your MongoDB connection details
 host = "13.127.57.185"
 port = 27017
 db = "pvvnl"
-username = "ml_user"
-password = "Project@3i2"
 collection = "predict"
 collection1 = "actual"
-
-# Escape username and password
-escaped_username = quote_plus(username)
-escaped_password = quote_plus(password)
-
-# Construct the MongoDB connection string
-MONGO_URL = f"mongodb://{escaped_username}:{escaped_password}@{host}:{port}/{db}"
+MONGO_URL = f"mongodb://{host}:{port}"
 client = MongoClient(MONGO_URL)
+
 db1 = client[db]
 collection_name = db1[collection]
-
+collection_name1 = db1[collection1]
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
--- routes/routes.py
+++ routes/routes.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from flask import Blueprint, request
-from config.db import collection_name
+
+from config.db import collection_name, collection_name1
 
 router = Blueprint('router', __name__)
 
@@ -7,14 +10,30 @@ router = Blueprint('router', __name__)
 # GET request
 @router.route('/getPredData', methods=['GET'])
 def get_todos():
-    # Access parameters from the query string
-    todo_id = request.args.get('id')
-    date = request.args.get('date')
+    try:
+        # Access parameters from the query string
+        todo_id = request.args.get('id')
+        date = request.args.get('date')
 
-    # Concatenate todo_id and date to create a new identifier
-    id = todo_id + "_" + date
-    query = {'_id': id}
+        # Concatenate todo_id and date to create a new identifier
+        id = todo_id + "_" + date
+        query = {'_id': id}
 
-    todos = list(collection_name.find(query))
-    return todos
+        todos = list(collection_name.find(query, {'_id': 0, 'data': 1}))
+        return todos
+    except Exception as e:
+        print(e)
 
+
+@router.route('/getActualData', methods=['POST'])
+async def get_actual_data(id: str, date: str):
+    try:
+        start_date = datetime.strptime(date, '%Y-%m-%d')
+        todos = list(collection_name1.find({
+            'sensor_id': id,
+            'Clock': start_date
+        }))
+
+        return todos
+    except Exception as e:
+        print(e)
