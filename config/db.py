164c75ae22bc0eff9bdc9e4eb136f5771c2f40cc
bhattshubham1401 <shubhambhatt687@gmail.com>
2023-12-04 16:07:33 +0530

    api

--- config/db.py
+++ config/db.py
@@ -4,11 +4,13 @@ host = "localhost"
 port = 27017
 db = "pvvnl"
 collection = "predict"
+collection1 = "actuals"
 MONGO_URL = f"mongodb://{host}:{port}"
 client = MongoClient(MONGO_URL)
 
 db1 = client[db]
 collection_name = db1[collection]
+collection_name1 = db1[collection1]
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
--- routes/routes.py
+++ routes/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
-from config.db import collection_name
+from config.db import collection_name, collection_name1
 
 router = Blueprint('router', __name__)
 
@@ -15,19 +15,27 @@ def get_todos():
 
         # Concatenate todo_id and date to create a new identifier
         id = todo_id + "_" + date
+        id1 = todo_id + ":" + date
         query = {'_id': id}
+        query1 = {'sensor_id': id1}
 
         todos = list(collection_name.find(query, {'_id': 0, 'data': 1}))
+        todos_act = list(collection_name1.find(query1, {'_id': 0, 'data': 1}))
         formatted_data = {"data": []}
         for key, value in todos[0]["data"].items():
             formatted_data["data"].append(value)
-
-        # Now, formatted_data has the structure you wanted
         payLoad = {"datas": formatted_data["data"]}
 
-        return payLoad
+        for key, value in todos_act[0]["data"].items():
+            formatted_data["data"].append(value)
+        payLoad1 = {"datas": formatted_data["data"]}
+
+        return jsonify(payLoad), jsonify(payLoad1)
+
+
     except Exception as e:
         print(e)
+        return jsonify({"error": str(e)}), 500
 
 
 # @router.route('/getActualData', methods=['POST'])
