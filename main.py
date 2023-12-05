b37e5fd5137712ffc672f333010f3c54e3dcbadb
bhattshubham1401 <shubhambhatt687@gmail.com>
2023-12-05 14:25:39 +0530

    api

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from routes.routes import router
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(router)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
