206253b1eca09a5a46967823c7d8b2a6ada386cd
bhattshubham1401 <shubhambhatt687@gmail.com>
2023-12-07 21:42:13 +0530

    api

--- routes/routes.py
+++ routes/routes.py
@@ -66,7 +66,8 @@ def get_sensorList():
                '641c17bc672215.97177522']
 
         # Convert the cursor to a list
-        todos_cursor = collection_name2.find({'id': {'$in': lst}}, {'id': 1, 'name': 1})
+        # todos_cursor = collection_name2.find({'id': {'$in': lst}}, {'id': 1, 'name': 1})
+        todos_cursor = collection_name2.find()
         todos = list(todos_cursor)
 
         # Return the data as JSON
