be9c426d90b72bb8ad8412a63b4917a121a2d234
bhattshubham1401 <shubhambhatt687@gmail.com>
2023-12-13 12:51:01 +0530

    artifact

--- routes/routes.py
+++ routes/routes.py
@@ -49,7 +49,7 @@ def get_todos():
         # Combine actual and predicted data into a single dictionary
         response_data = {"actual_data": actual_data["data_act"], "predicted_data": predicted_data["data_pred"]}
 
-        return {"data": response_data}
+        return {"rc": 0 ,"message":"Success", "data": response_data}
 
     except Exception as e:
         return {"error": str(e)}
@@ -58,27 +58,36 @@ def get_todos():
 @router.route('/get_sensorList', methods=['GET'])
 def get_sensorList():
     try:
-        lst = ['5f718b613291c7.03696209', '5f718c439c7a78.65267835', '614366bce31a86.78825897',
-               '6148740eea9db0.29702291', '62307a944c9117.27764752', '625fb44c5fb514.98107900',
-               '625fb9e020ff31.33961816', '6260fd4351f892.69790282', '627cd4815f2381.31981050',
-               '629087dedbd477.79790710', '629094ee5fdff4.43505210', '6295bdace55341.17149388',
-               '6295eb61511b31.65607460', '62a9920f75c931.62399458', '62a9d0d7af97e3.16097779',
-               '62aad7f5c65185.80723547', '62b15dfee341d1.73837476', '62b595eabd9df4.71374208',
-               '6349368c306542.16235883', '634e7c43038801.39310596', '6399a18b1488b8.07706749',
-               '63a413c88f4716.77874329', '63a4195534d625.00718490', '63a4272631f153.67811394',
-               '63aa9161b9e7e1.16208626', '63aaca5d76b0e8.04988241', '63ca403ccd66f3.47133508',
-               '641c17bc672215.97177522']
+        sensor_ids = [
+            '5f718b613291c7.03696209', '5f718c439c7a78.65267835', '614366bce31a86.78825897',
+            '6148740eea9db0.29702291', '62307a944c9117.27764752', '625fb44c5fb514.98107900',
+            '625fb9e020ff31.33961816', '6260fd4351f892.69790282', '627cd4815f2381.31981050',
+            '629087dedbd477.79790710', '629094ee5fdff4.43505210', '6295bdace55341.17149388',
+            '6295eb61511b31.65607460', '62a9920f75c931.62399458', '62a9d0d7af97e3.16097779',
+            '62aad7f5c65185.80723547', '62b15dfee341d1.73837476', '62b595eabd9df4.71374208',
+            '6349368c306542.16235883', '634e7c43038801.39310596', '6399a18b1488b8.07706749',
+            '63a413c88f4716.77874329', '63a4195534d625.00718490', '63a4272631f153.67811394',
+            '63aa9161b9e7e1.16208626', '63aaca5d76b0e8.04988241', '63ca403ccd66f3.47133508',
+            '641c17bc672215.97177522'
+        ]
 
-        # Convert the cursor to a list
-        todos_cursor = collection_name2.find({'id': {'$in' : lst}}, {'_id': 0, 'id': 1, 'name': 1})
-        todos = [{'id': doc['id'], 'name': doc['name']} for doc in todos_cursor]
+        url = "https://multipoint.myxenius.com/Sensor_newHelper/getDataApi"
+        params = {
+            'sql': "select id uuid, name sensorName from sensor where id in ({}) ORDER BY name".format(','.join(f"'{sid}'" for sid in sensor_ids)),
+            'type': 'query'
+        }
 
-        # Return the data as JSON
-        return jsonify({'sensorList': todos})
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+
+        sensor_list = [{'uuid': item['uuid'], 'sensorName': item['sensorName']} for item in data['resource']]
+
+        return jsonify({"rc": 0, "message": "Success", 'sensorList': sensor_list})
+
     except Exception as e:
         print(e)
         return jsonify({"error": "An error occurred"}), 500
-
 @router.route('/getPredDataHourly', methods=['GET'])
 def getPredDataHourly():
     try:
@@ -157,7 +166,7 @@ def getPredDataHourly():
         # Combine actual and predicted data into a single dictionary
         response_data = {"actual_data": actual_data["data_act"], "predicted_data": predicted_data["data_pred"]}
 
-        return {"data": response_data}
+        return {"rc": 0, "message":"Success", "data": response_data}
 
     except Exception as e:
         return {"error": str(e)}
@@ -260,7 +269,7 @@ def getPredDataDaily():
         # Combine actual and predicted data into a single dictionary
         response_data = {"actual_data": actual_data["data_act"], "predicted_data": predicted_data["data_pred"]}
 
-        return {"data": response_data}
+        return {"rc": 0 ,"message":"Success", "data": response_data}
 
     except Exception as e:
         return {"error": str(e)}
