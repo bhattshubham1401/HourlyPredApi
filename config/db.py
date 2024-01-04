a836b7b38c2a051bed53c58a24e3eaad2004b08a
bhattshubham1401 <shubhambhatt687@gmail.com>
2024-01-04 15:02:11 +0530

    Testing

--- config/db.py
+++ config/db.py
@@ -5,9 +5,11 @@ host = "localhost"
 port = 27017
 db = "pvvnl"
 collection = "predictTest"
-# collection = "prediction_h"
 collection1 = "actual"
 collection2 = "sensor"
+collection3 = "prediction_h"
+collection4 =  "predict"
+
 MONGO_URL = f"mongodb://{host}:{port}"
 client = MongoClient(MONGO_URL)
 
@@ -15,6 +17,8 @@ db1 = client[db]
 collection_name = db1[collection]
 collection_name1 = db1[collection1]
 collection_name2 = db1[collection2]
+collection_name3 = db1[collection3]
+collection_name4 = db1[collection4]
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
