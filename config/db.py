f08e9d5f9bdd0de7da0719f52d3b00e7ed67ded7
harshvardhan sharma <harshvardhan.sharma@radius.co.in>
2024-01-04 12:25:47 +0530

    addition testing api

--- config/db.py
+++ config/db.py
@@ -1,12 +1,15 @@
 from pymongo.mongo_client import MongoClient
 
-host = "13.127.57.185"
-# host = "localhost"
+# host = "13.127.57.185"
+host = "localhost"
 port = 27017
 db = "pvvnl"
-collection = "predict"
+collection = "predictTest"
 collection1 = "actual"
 collection2 = "sensor"
+collection3 = "prediction_h"
+collection4 =  "predict"
+
 MONGO_URL = f"mongodb://{host}:{port}"
 client = MongoClient(MONGO_URL)
 
@@ -14,6 +17,8 @@ db1 = client[db]
 collection_name = db1[collection]
 collection_name1 = db1[collection1]
 collection_name2 = db1[collection2]
+collection_name3 = db1[collection3]
+collection_name4 = db1[collection4]
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
