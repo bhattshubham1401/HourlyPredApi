8fa74ee0d59f82f59674f4cc33b0f638acc33150
bhattshubham1401 <shubhambhatt687@gmail.com>
2024-01-08 11:52:17 +0530

    Testing

--- config/db.py
+++ config/db.py
@@ -6,13 +6,19 @@ port = 27017
 db = "pvvnl"
 collection = "predictTest"
 collection1 = "forcast"
-collection2 =  "prediction_h"
-collection3 =  "predict"
+collection2 = "prediction_h"
+collection3 = "predict"
 
 MONGO_URL = f"mongodb://{host}:{port}"
 client = MongoClient(MONGO_URL)
 
 db1 = client[db]
+'''
+collection is for condition including holidays and weather data 
+collection1 is for condition without holiday and weather
+collection2 is for condition with simple load profile data
+collection3 is for condition including holidays, weather data also make stationary and correlation` 
+'''
 collection_name = db1[collection]
 collection_name1 = db1[collection1]
 collection_name2 = db1[collection2]
