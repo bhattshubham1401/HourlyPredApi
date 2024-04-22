f4a23e7787415f624b24e7cc21dc0859a78d525b
bhattshubham1401 <shubhambhatt687@gmail.com>
2024-04-22 11:42:53 +0530

    db

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 CORS(app)
 app.register_blueprint(router)
 
+print(app.register_blueprint(router))
+
 if __name__ == "__main__":
     app.run(debug=True)
     # app.run(debug=True, host="127.0.0.1", port=5000)
