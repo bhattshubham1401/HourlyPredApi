40f490524c0c6778cd9ce73b647471e7db611e21
bhattshubham1401 <shubhambhatt687@gmail.com>
2024-04-22 10:43:53 +0530

    db

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ CORS(app)
 app.register_blueprint(router)
 
 if __name__ == "__main__":
-    # app.run(debug=True,host="0.0.0.0", port=5000)
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True,host="0.0.0.0", port=5000)
+    # app.run(debug=True, host="127.0.0.1", port=5000)
