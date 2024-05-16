34f21554eb54126d23087873a31b004d81e6ef97
Aniket singh <71018641+Aniketsuraj@users.noreply.github.com>
2024-05-16 18:45:33 +0530

    api

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ app.register_blueprint(router)
 # print(app.register_blueprint(router))
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True)
+    # app.run(debug=True, host="127.0.0.1", port=5000)
