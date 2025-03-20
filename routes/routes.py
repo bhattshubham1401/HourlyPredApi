728d1320bfb1918b0e195d4f5b99ab4b76b6c27a
bhattshubham1401 <shubhambhatt687@gmail.com>
2025-03-20 12:27:04 +0530

    add adata

--- routes/routes.py
+++ routes/routes.py
@@ -2,9 +2,7 @@ import concurrent
 from calendar import monthrange
 from concurrent.futures import ThreadPoolExecutor
 
-import pymongo
 from flask import Blueprint, request, jsonify
-from pymongo import InsertOne, UpdateOne, errors
 
 from config.db import collection_name, collection_name1, collection_name2, collection_name3, collection_name4, \
     collection_name5, collection_name6, collection_name7, collection_name8, collection_name9, collection_name10, collection_name13
@@ -1915,16 +1913,19 @@ def getPredDataDailyjpdcl():
 @router.route('/getweatherdataV1', methods=['POST'])
 def getweatherdataV1():
     try:
+        # Get JSON data from request
         data = request.get_json()
+        # Validate input parameters
         if not data or 'site_id' not in data or not isinstance(data['site_id'], list):
             return jsonify({"error": "Invalid input. Expected a list of site_id."}), 400
         if 'start_date' not in data or 'end_date' not in data:
             return jsonify({"error": "Missing start_date or end_date."}), 400
 
-        lst = data['site_id']
+        lst = data['site_id']  # Use the passed list
         start_date = data['start_date']
         end_date = data['end_date']
 
+        # Validate date format (YYYY-MM-DD)
         try:
             datetime.strptime(start_date, "%Y-%m-%d")
             datetime.strptime(end_date, "%Y-%m-%d")
@@ -1938,82 +1939,14 @@ def getweatherdataV1():
                 "latitude": {"$min": "$latitude"},
                 "longitude": {"$min": "$longitude"},
                 "sensors": {
-                    "$addToSet": {"id": "$id", "name": "$name", "latitude": "$latitude", "longitude": "$longitude"}
-                }
-            }}
-        ]
-
-        result = list(collection_name7.aggregate(pipeline))
-        bulk_insert_data = []
-
-        for site_data in result:
-            url = f"https://archive-api.open-meteo.com/v1/archive?latitude={site_data['latitude']}&longitude={site_data['longitude']}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,wind_speed_10m,wind_speed_100m"
-            print(url)
-            response = requests.get(url)
-            response.raise_for_status()
-            weather_data = response.json()
-
-            if "hourly" in weather_data:
-                for i in range(len(weather_data['hourly']['time'])):
-                    hour_data = {
-                        "_id": f"{site_data['_id']}_{weather_data['hourly']['time'][i]}",
-                        "site_id": site_data["_id"],
-                        "time": weather_data['hourly']['time'][i],
-                        "temperature_2m": weather_data['hourly'].get('temperature_2m', [])[i],
-                        "relative_humidity_2m": weather_data['hourly'].get('relative_humidity_2m', [])[i],
-                        "apparent_temperature": weather_data['hourly'].get('apparent_temperature', [])[i],
-                        "precipitation": weather_data['hourly'].get('precipitation', [])[i],
-                        "wind_speed_10m": weather_data['hourly'].get('wind_speed_10m', [])[i],
-                        "wind_speed_100m": weather_data['hourly'].get('wind_speed_100m', [])[i],
-                        "creation_time_iso": datetime.utcfromtimestamp(
-                            datetime.strptime(weather_data['hourly']['time'][i], '%Y-%m-%dT%H:%M').timestamp()).isoformat()
-                    }
-                    bulk_insert_data.append(InsertOne(hour_data))
-
-        if bulk_insert_data:
-            try:
-                collection_name8.bulk_write(bulk_insert_data, ordered=False)
-                return {"message": "Weather data fetched and stored successfully"}
-            except errors.BulkWriteError as bwe:
-                print(f"Bulk write error: {bwe.details}")
-                return {"message": "Weather data fetched, but some duplicates were ignored."}
-        else:
-            return {"message": "No weather data available for the specified sites"}
-
-    except Exception as e:
-        return {"error": str(e)}
-
-
-@router.route('/getweatherdataV2', methods=['POST'])
-def getweatherdatav2():
-    try:
-        data = request.get_json()
-        if not data or 'site_id' not in data or not isinstance(data['site_id'], list):
-            return jsonify({"error": "Invalid input. Expected a list of site_id."}), 400
-        if 'start_date' not in data or 'end_date' not in data:
-            return jsonify({"error": "Missing start_date or end_date."}), 400
-
-        lst = data['site_id']
-        start_date = data['start_date']
-        end_date = data['end_date']
-
-        pipeline = [
-            {"$match": {'type': 'AC', 'admin_status': {"$in": ['N', 'S', 'U']},
-                        'site_id': {"$in": lst}}},
-            {"$group": {
-                "_id": "$site_id",
-                "latitude": {"$min": "$latitude"},
-                "longitude": {"$min": "$longitude"},
-                "sensors": {
                     "$addToSet": {"id": "$id", "name": "$name", "latitude": "$latitude", "longitude": "$longitude"}}
             }}
         ]
 
         result = list(collection_name7.aggregate(pipeline))
-        print(result)
-
         bulk_insert_data = []
 
+        # Iterate over each site
         for site_data in result:
             url = f"https://archive-api.open-meteo.com/v1/archive?latitude={site_data['latitude']}&longitude={site_data['longitude']}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,wind_speed_10m,wind_speed_100m"
             print(url)
@@ -2021,10 +1954,11 @@ def getweatherdatav2():
             response.raise_for_status()
             weather_data = response.json()
 
+            # Process weather data if available
             if "hourly" in weather_data:
                 for i in range(len(weather_data['hourly']['time'])):
                     hour_data = {
-                        "_id": f"{site_data['_id']}_{weather_data['hourly']['time'][i]}",
+                        "_id": f"{site_data['_id']}_{weather_data['hourly']['time'][i]}",  # MongoDB's unique identifier
                         "site_id": site_data["_id"],
                         "time": weather_data['hourly']['time'][i],
                         "temperature_2m": weather_data['hourly'].get('temperature_2m', [])[i],
@@ -2034,21 +1968,24 @@ def getweatherdatav2():
                         "wind_speed_10m": weather_data['hourly'].get('wind_speed_10m', [])[i],
                         "wind_speed_100m": weather_data['hourly'].get('wind_speed_100m', [])[i],
                         "creation_time_iso": datetime.utcfromtimestamp(
-                            datetime.strptime(weather_data['hourly']['time'][i], '%Y-%m-%dT%H:%M').timestamp()).isoformat()
+                            datetime.strptime(weather_data['hourly']['time'][i],
+                                              '%Y-%m-%dT%H:%M').timestamp()).isoformat()
                     }
+
                     bulk_insert_data.append(hour_data)
 
+                    # print(bulk_insert_data)
+
+        # Insert the data into MongoDB in bulk
         if bulk_insert_data:
-            try:
-                collection_name8.insert_many(bulk_insert_data, ordered=False)
-            except pymongo.errors.BulkWriteError as e:
-                print(f"Duplicate entries found and skipped: {e.details}")
-            return {"message": "Weather data fetched and stored successfully (duplicates skipped)"}
+            collection_name8.insert_many(bulk_insert_data)
+            return {"message": "Weather data fetched and stored successfully"}
         else:
             return {"message": "No weather data available for the specified sites"}
 
     except Exception as e:
         return {"error": str(e)}
+
 
 # @router.route('/get_data', methods=['POST'])
 # def get_data():
